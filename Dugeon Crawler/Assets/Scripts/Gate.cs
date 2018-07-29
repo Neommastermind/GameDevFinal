5f7135e8258b8999b629f6d389b6e589024a995f
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-07-29 01:12:44 -0600

    Door and torches edit
    The door now closes when you step through it, and the torches are better.

---
+++ Dugeon Crawler/Assets/Scripts/Gate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            GetComponentInParent<Animator>().SetTrigger("Lower");
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
