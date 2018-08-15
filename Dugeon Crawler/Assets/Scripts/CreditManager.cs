483355a0922c45d3110db8069b83d8e4c0c96d8b
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-08-15 16:15:25 -0600

    Basic End scrren created.
    Created a basic ending screen for the beta.

---
+++ Dugeon Crawler/Assets/Scripts/CreditManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Return to the main menu
+        StartCoroutine("ReturnToMainMenu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//TODO: After beta add credits
+	}
+
+    IEnumerator ReturnToMainMenu()
+    {
+        //Return to the main menu after some time
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
--- Dugeon Crawler/Assets/Scripts/FloorExit.cs
+++ Dugeon Crawler/Assets/Scripts/FloorExit.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class FloorExit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            //TODO: Change this to another floor if we ever go beyond beta.
+            //End screen for beta
+            SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+        }
+    }
 }
